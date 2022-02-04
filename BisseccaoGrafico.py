1aa5ae5b1928a05326c118fd08a425ed235b0a01
Rafa <arruda.rafa.ra@gmail.com>
2022-02-04 16:23:46 -0300

    atualizando atividade de calculo numerico

--- BisseccaoGrafico.py
+++ BisseccaoGrafico.py
@@ -3,16 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x):
-    return x + 3*math.cos(x) - np.exp(x) # mudamos aqui a expressão dada da questão para obtermos o resultado do calculo
+    #return 0.4*x**3 - np.exp(2*x) + 1.8
+    return x**3 - 4 # mudamos aqui a expressão dada da questão para obtermos o resultado do calculo
 
-#a = -2#intervalo, a < b
-#b = -1
-
+#a = 1 #intervalo, a < b
+#b = 2
 a = float(input("Entre com o valor de 'a' do intervalo [a;b]: "))
 b = float(input("Entre com o valor de 'b' do intervalo [a;b]: "))
 
-
-eps = 10**(-2)#0.000001
+eps = 10**(-6)#0.000001
 i = 0
 y = []
 eixoX = []
@@ -24,17 +23,15 @@ for i in range(n):
     eixoX.append(0)
 
 
-
-
 #existencia da raíz no intervalo
-def verificaRaiz(a, x0):
-    if(f(a)*f(x0) <= 0):
+def verificaRaiz(a, b):
+    if(f(a)*f(b) <= 0):
         return True
     else:
         return False
 
 #codigo principal
-def cordas(a, b):
+def bisseccao(a, b):
     
     if verificaRaiz(a, b):
         print ('Precisão: {:.10f}'.format(eps))
@@ -42,12 +39,12 @@ def cordas(a, b):
         print ('\nProcesso de Convergencia:\n')
         cont_it = 0
         fx0 = 100
-        x0 = (a*f(b)-b*f(a))/(f(b)-f(a))
-        fa = f(a)
-        
+
         while abs(fx0) > eps:
+            x0 = ((b+a)/2.0) #+ a #atualiza ponto médio
             cont_it += 1 #contador de iterações
             fx0 = f(x0)
+            
 
             #verifica se o próprio ponto é a raíz
             if(f(a) == 0):
@@ -63,12 +60,11 @@ def cordas(a, b):
             if verificaRaiz(a, x0):
                 b = x0
             else:
+                verificaRaiz(x0, b)
                 a = x0
-                
-
-            x0 = (a*f(b)-b*f(a))/(f(b)-f(a))
+            
             print ('I{} = {:.10f}'.format(cont_it, x0))
-          
+            
 
     else:
         print ('Função não possui raíz no intervalo selecionado.')
@@ -79,11 +75,9 @@ def cordas(a, b):
     print ('\nQuantidade de iterações processadas: {}'.format(cont_it))
     print ('Valor final: {:.10f}'.format(x0))
     plt.plot(x,y,'b',x,eixoX,'black')
-    plt.show() 
-
+    plt.show()  
 
 if(a >= b):
     print ('Intervalo inválido.')
-
 else:
-    cordas(a, b)
+    bisseccao(a, b)
---
+++ InterpolacaoLagrange.py
@@ -0,0 +1,34 @@
+# para entender melhor a logica deste codigo, leia este aqui artigo(link)
+# https://cn.ect.ufrn.br/index.php?r=conteudo%2Finterp-lagrange
+
+#Metodo interpolacao lagrange em python 3
+
+def main():
+    pontos = int(input('Quantidade de pontos que serão utilizados: '))
+    X,Y =[],[]
+    for i in range(pontos):
+        x=float(input("X" + str(i)+ "="))
+        X.append(x)
+
+        y = float(input("Y" + str(i)+ "="))
+        Y.append(y)
+
+    x=float(input("Valor que se deseja interpolar:"))
+    coefientes = []
+
+    for indice in range(pontos):
+        L=1
+        for j in range(len(X)):
+            if indice !=j:
+                L*=(x-X[j]) / (X[indice]-X[j])
+        coefientes.append(L)
+    
+    pn=0
+    for i in range(len(coefientes)):
+        pn +=Y[i]* coefientes[i]
+    
+    print("p("+ str(x)+ ")=", pn)
+
+if __name__ == '__main__':
+	main()
+
